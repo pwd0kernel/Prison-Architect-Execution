7a92e799b2e039714866221000647cb4974b5b9d
pwd0kernel <79817991+pwd0kernel@users.noreply.github.com>
2021-08-16 17:32:28 -0230

    Update Console.h

--- Codes 1.04/Console.h
+++ Codes 1.04/Console.h
@@ -1,19 +1,26 @@
-#pragma once
-#include <Windows.h>
-#include <stdio.h>
-#include <iostream>
+//
+// Created by pwd0kernel on 8/15/2021.
+//
+
+#ifndef UNTITLED14_CONSOLE_H
+#define UNTITLED14_CONSOLE_H
+
+#include <consoleapi2.h>
+#include <cstdio>
 
 class Console {
 private:
 public:
-	static void AllowConsole(const char* name) {
-		AllocConsole();
-		if (freopen("CONOUT$", "w", stdout));
-		if (freopen("CONIN$", "r", stdin));
-		SetConsoleTitleA(name);
-	};
+    static void AllowConsole(const char *name) {
+        AllocConsole();
+        if (freopen("CONOUT$", "w", stdout));
+        if (freopen("CONIN$", "r", stdin));
+        SetConsoleTitleA(name);
+    };
 
-	void ConsoleOutput(const char* text) {
-		std::cout << "[Console]: " << text << std::endl;
-	}
+    static void ConsoleOutput(const char *text) {
+        printf("[Console]: %s %s", text  , "\n");
+    }
 };
+
+#endif //UNTITLED14_CONSOLE_H
